df3eda259f4b33db0cc114c58c17ef09a208b0e5
Gabo <beliczky3@gmail.com>
2024-12-05 08:31:17 +0100

    html id-k valtoztatasa + js

--- js.js
+++ js.js
@@ -1,15 +1,29 @@
 const kezdooldal = document.getElementById("kezdo")
-const futamoldal = document.getElementById("futam")
+const futamoldal = document.getElementById("fo")
 const eredmenyoldal = document.getElementById("eredmeny")
+const csapat1neveh = document.getElementById("hazaicsapatneve")
+const csapat2neveh = document.getElementById("vendegcsapatneve")
+let originalheightf = futamoldal.style.height
+let originalheightk = kezdooldal.style.height
+let originalheighte = eredmenyoldal.style.height
 let csapat1neve
 let csapat2neve
 let felidokhossza
 let elteltido
 let szamolo
+futamoldal.style.visibility = "hidden"
+eredmenyoldal.style.visibility = "hidden"
+futamoldal.style.height = "0px"
+eredmenyoldal.style.height = "0px"
 function adatbeadas()
 {
-    csapat1neve = document.getElementById("csapat1nevinput").Value
-    csapat2neve = document.getElementById("csapat2nevinput").Value
+    futamoldal.style.visibility = "visible"
+    futamoldal.style.height = originalheightf
+    kezdooldal.style.visibility = "hidden"
+    kezdooldal.style.height = "0px"
+    csapat1neve = document.getElementById("csapat1").value
+    csapat2neve = document.getElementById("csapat2").value
+    /*
     felidokhossza = parseInt(document.getElementById("percinput").Value*60+document.getElementById("mpinput").Value)
     document.getElementById("mpinput").Value = 0
     document.getElementById("percinput").Value = 0
@@ -19,5 +33,23 @@ function adatbeadas()
     document.getElementById("csapat2neve").innerText = csapat2neve
     document.getElementById("idohatra").innerText = (Math.floor(elteltido / 60) + ":" + elteltido % 60)
     kezdooldal.style.visibility = "hidden"
-    futamoldal.style.visibility = "visible"
+    futamoldal.style.visibility = "visible"*/
+}
+function vegeredmeny()
+{
+    futamoldal.style.visibility = "hidden"
+    futamoldal.style.height = "0px"
+    eredmenyoldal.style.visibility = "visible"
+    eredmenyoldal.style.height = originalheighte
+    csapat1neveh.innerText = csapat1neve
+    csapat2neveh.innerText = csapat2neve
+}
+function restart()
+{
+    document.getElementById("csapat1").value = ""
+    document.getElementById("csapat2").value = ""
+    eredmenyoldal.style.visibility = "hidden"
+    eredmenyoldal.style.height = "0px"
+    kezdooldal.style.visibility = "visible"
+    kezdooldal.style.height = originalheighte
 }
